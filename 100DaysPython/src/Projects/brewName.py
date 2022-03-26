b24a016274c54e887fcc492bb6bf2e6dd344a0f0
Rajdeep <89920681+AiseKaise@users.noreply.github.com>
2022-03-26 19:23:47 +0530

    Add files via upload

---
+++ 100DaysPython/src/Projects/brewName.py
@@ -0,0 +1,9 @@
+# Welcome Text 
+print("\n ***** Welcome to the band name generator. ****** \n\n ")
+
+# Taking the input from the user
+city = input("Which city did you grew up in?\n" )
+pet = input("What is the name of you pet?\n")
+
+# Printing the final output
+print("Your band name could be " + city + " " + pet )
