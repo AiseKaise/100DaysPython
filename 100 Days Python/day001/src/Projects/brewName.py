1c0575d7812fb22bb4d881f2f6bbc0ea373f5419
Rajdeep <89920681+AiseKaise@users.noreply.github.com>
2022-03-26 19:26:16 +0530

    Delete 100 Days Python/day001/src directory

--- 100 Days Python/day001/src/Projects/brewName.py
+++
@@ -1,9 +0,0 @@
-# Welcome Text 
-print("\n ***** Welcome to the band name generator. ****** \n\n ")
-
-# Taking the input from the user
-city = input("Which city did you grew up in?\n" )
-pet = input("What is the name of you pet?\n")
-
-# Printing the final output
-print("Your band name could be " + city + " " + pet )
