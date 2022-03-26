75d3f74e984d551c7b6afc66197b4f83f8af6e4b
Rajdeep <89920681+AiseKaise@users.noreply.github.com>
2022-03-26 19:25:36 +0530

    Add files via upload

---
+++ 100 Days Python/day001/src/input.py
@@ -0,0 +1,8 @@
+# Taking the input
+input("What is your name? ")
+
+# Printing the size of the value
+print(len("Angela"))
+
+# Printing the Size of input value given by the user 
+print( len( input("What is your name ?") ) )
